e998d4628eedc0499fcdcddb0c68baf50708bc0e
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-07-07 00:56:49 +0900

    어드레서블 예제

--- Assets/Scripts/Addressable/AddressableManager.cs
+++ Assets/Scripts/Addressable/AddressableManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets; // using Addressable
 
 public class AddressableManager : MonoBehaviour
 {
+    // 어드레서블 타입 선언
     [SerializeField] private AssetReferenceGameObject cubeObj;
     [SerializeField] private AssetReferenceGameObject planeObj;
+    [SerializeField] private AssetReferenceSprite coinSprite;
 
+    [SerializeField] private Image image;
 
-    [SerializeField]private List<GameObject> gameObjs = new List<GameObject>();
+    // 생성된 오브젝트들을 저장하는 리스트
+    private List<GameObject> gameObjs = new List<GameObject>();
 
     void Start()
     {
@@ -22,7 +27,7 @@ public class AddressableManager : MonoBehaviour
         yield return init;
     }
 
-    public void Button_SpawnObject()
+    public void Button_SpawnObject() // Load
     {
         cubeObj.InstantiateAsync().Completed += (obj) =>
         {
@@ -33,15 +38,23 @@ public class AddressableManager : MonoBehaviour
         {
             gameObjs.Add(obj.Result);
         };
+
+        coinSprite.LoadAssetAsync().Completed += (img) =>
+        {
+            image.sprite = img.Result;
+        };
     }
 
-    public void Button_ReleaseObject()
+    public void Button_ReleaseObject() // Release
     {
         if(gameObjs.Count == 0)
         {
             return;
         }
 
+        coinSprite.ReleaseAsset();
+        image.sprite = null;
+        
         for(int i = gameObjs.Count - 1; i >= 0; i--)
         {
             Addressables.ReleaseInstance(gameObjs[i]);
